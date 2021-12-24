e5036df6e3604aa0dd5d6b5c7d2369b4036b9d67
Rafael Viana <rafaelvinicius@gmail.com>
2021-12-24 09:48:56 -0300

    Projeto spring admin. Para monitorar o estado das aplicações. - Spring admin project. To monitor the status of applications.

---
+++ mylearning_java-spring-spring-admin/src/test/java/br/com/mylearnings/monitoramentospringbootadmin/MonitoramentoSpringbootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mylearnings.monitoramentospringbootadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentoSpringbootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
