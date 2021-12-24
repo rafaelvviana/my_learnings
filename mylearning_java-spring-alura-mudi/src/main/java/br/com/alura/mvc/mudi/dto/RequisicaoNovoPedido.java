cdbfd5ce7b7cd18c579a97c1738439229feb7b3b
Rafael Viana <rafaelvinicius@gmail.com>
2021-12-24 09:42:12 -0300

    Projeto spring boot, Alura Mudi. Ofertas de produtos online. - Spring boot project, Alura Mudi. online produt offering.

---
+++ mylearning_java-spring-alura-mudi/src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -0,0 +1,55 @@
+package br.com.alura.mvc.mudi.dto;
+
+import javax.validation.constraints.NotBlank;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.model.StatusPedido;
+
+public class RequisicaoNovoPedido {
+
+	@NotBlank // NotBlank.RequisicaoNovoPedido.nomeProduto ==>> erro
+	private String nomeProduto;
+	@NotBlank
+	private String urlProduto;
+	@NotBlank
+	private String urlImagem;
+	@NotBlank
+	private String descricao;
+	
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public String getUrlProduto() {
+		return urlProduto;
+	}
+	public void setUrlProduto(String urlProduto) {
+		this.urlProduto = urlProduto;
+	}
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public Pedido toPedido() {
+		Pedido pedido = new Pedido();
+		pedido.setNomeProduto(nomeProduto);
+		pedido.setDescricao(descricao);
+		pedido.setUrlImagem(urlImagem);
+		pedido.setUrlProduto(urlProduto);
+		pedido.setStatus(StatusPedido.AGUARDANDO);
+		
+		return pedido;
+	}
+	
+}
---
+++ mylearning_java-spring-alura-mudi/src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -0,0 +1,38 @@
+package br.com.alura.mvc.mudi.repository;
+
+import java.util.List;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.model.StatusPedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+	@Cacheable("pedidos")
+	List<Pedido> findByStatus(StatusPedido aguardando, Pageable page);
+
+	@Query("SELECT p FROM Pedido p JOIN p.user u WHERE u.username = :username")
+	List<Pedido> findAllByUsuario(@Param("username") String username);
+
+	@Query("SELECT p FROM Pedido p JOIN p.user u WHERE u.username = :username and p.status = :status")
+	List<Pedido> findByStatusEUsuario(@Param("status") StatusPedido status, @Param("username") String username);
+	
+}
+
+//@Repository
+//public class PedidoRepository {
+//	@PersistenceContext
+//	private EntityManager entityManager;
+	
+//	public List<Pedido> recuperaTodosOsPedidos() {
+//		Query query = entityManager.createQuery("select p from Pedido p");
+//		return query.getResultList();
+//	}
+//}
